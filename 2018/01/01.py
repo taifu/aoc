ba643fec57bc0164ddb9901f695f70e3a439be36
Marco Beri <marcoberi@gmail.com>
2018-12-01 13:38:21 +0100

    2018

---
+++ 2018/01/01.py
@@ -0,0 +1,12 @@
+freqs = [int(x.strip()) for x in open("input").readlines()]
+print(sum(freqs))
+curr_freq = 0
+seen = set([0])
+n = 0
+while True:
+    curr_freq += freqs[n % len(freqs)]
+    if curr_freq in seen:
+        print(curr_freq)
+        break
+    seen.add(curr_freq)
+    n += 1
