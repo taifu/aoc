f3cc6a5eea2f2b5d659a441a3e4b4e6d640817f8
Marco Beri <marcoberi@gmail.com>
2018-12-05 15:21:32 +0100

    05 2018

---
+++ 2018/05/05.py
@@ -0,0 +1,20 @@
+import re
+
+
+data = open("input.txt").read().strip()
+
+
+def react(data):
+    p = 0
+    while p < len(data) - 1:
+        if data[p].lower() == data[p + 1].lower() and data[p] != data[p + 1]:
+            data = data[:p] + data[p + 2:]
+            if p > 0:
+                p -= 1
+        else:
+            p += 1
+    return data
+
+
+print(len(react(data)))
+print(min(len(react(re.sub(c, '', data, flags=re.IGNORECASE))) for c in set(data.lower())))
