71c733a9c915745c76fabfffd6f2f365774bbcb7
Marco Beri <marcoberi@gmail.com>
2018-12-18 19:32:50 +0100

    18 fixed

--- 2018/18/18.py
+++ 2018/18/18.py
@@ -18,13 +18,14 @@ GROUND, TREE, LUMBER = ".", "|", "#"
 
 class Life:
     def __init__(self, raw):
-        self._read(raw)
+        self.raw = raw
 
-    def _read(self, raw):
+    def _reset(self):
         self.map = []
-        for line in raw.split("\n"):
+        for line in self.raw.split("\n"):
             if line:
                 self.map.append(list(line))
+        self._value = 0
         self.width = len(self.map[0])
         self.height = len(self.map)
 
@@ -56,45 +57,58 @@ class Life:
 
     @property
     def value(self):
-        trees = lumbers = 0
-        for y in range(self.height):
-            for x in range(self.width):
-                if self.map[y][x] == TREE:
-                    trees += 1
-                elif self.map[y][x] == LUMBER:
-                    lumbers += 1
-        return trees * lumbers
+        return self._value
 
     def evolve(self):
         map2 = []
+        trees = lumbers = 0
+        self._key = ""
         for y in range(self.height):
             map2.append(["."] * self.width)
             for x in range(self.width):
-                map2[y][x] = self.cell(x, y)
+                next_cell = self.cell(x, y)
+                if next_cell == TREE:
+                    trees += 1
+                elif next_cell == LUMBER:
+                    lumbers += 1
+                map2[y][x] = next_cell
+            self._key += "".join(map2[y])
+        self._value = trees * lumbers
         self.map = map2
 
+    def go(self, turn, show=False):
+        self._reset()
+        seen = {}
+        values = {}
+        if show:
+            game.draw()
+        for n in range(turn):
+            game.evolve()
+            values[n] = self._value
+            try:
+                last = seen[self._key]
+                return values[last - 1 + (turn - n) % (n - last)]
+            except KeyError:
+                seen[self._key] = n
+            if show:
+                game.draw()
+        return(game.value)
 
-game = Life(raw_example)
-game.draw()
-for n in range(10):
-    game.evolve()
-game.draw()
-print(game.value)
+
+# game = Life(raw_example)
+# print(game.go(10))
 
 game = Life(raw)
-game.draw()
-for n in range(10000000):
-    game.evolve()
-    if n > 100:
-        print(n + 1, game.value)
-game.draw()
-print(game.value)
+print(game.go(10))
 
-if seen
->>> (1000000000 - 1097)
-999998903
->>> (1000000000 - 1097) // 28
-35714246
->>> (1000000000 - 1097) % 28
-15
+game = Life(raw)
+print(game.go(1000000000))
+
+# if seen
+# >>> (1000000000 - 1097)
+# 999998903
+# >>> (1000000000 - 1097) // 28
+# 35714246
+# >>> (1000000000 - 1097) % 28
+# 15
 
