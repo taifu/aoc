5b4936d226b6ac3675ebcee675c44cea22aef1a3
Marco Beri <marcoberi@gmail.com>
2018-12-23 03:25:32 +0100

    22 both

--- 2018/22/22.py
+++ 2018/22/22.py
@@ -1,3 +1,6 @@
+from heapq import heappush, heappop
+
+
 class Point:
     def __init__(self, x, y):
         '''Defines x and y variables'''
@@ -38,6 +41,25 @@ class Point:
         return hash((self.x, self.y))
 
 
+class PointTool:
+    def __init__(self, point, tool):
+        '''Defines x and y variables'''
+        self.point = point
+        self.tool = tool
+
+    def __lt__(self, other):
+        return self.point.x < other.point.y or self.point.y < other.point.y
+
+    def __eq__(self, other):
+        return self.point == other.point and self.tool == other.tool
+
+    def __hash__(self):
+        return hash((self.point, self.tool))
+
+    def __repr__(self):
+        return "{} {},{}".format(self.tool, self.point.x, self.point.y)
+
+
 ROCKY = '.'
 WET = '='
 NARROW = '|'
@@ -52,8 +74,8 @@ class Cave:
         self._build()
 
     def _build(self):
-        self.width = self.target.x - self.origin.x + 500
-        self.height = self.target.y - self.origin.y + 500
+        self.width = self.target.x - self.origin.x + 50
+        self.height = self.target.y - self.origin.y + 10
         erosions = {}
         self.risk = 0
         self._map = {}
@@ -95,74 +117,81 @@ class Cave:
             return NEITHER, TORCH
         return TORCH, CLIMBING
 
-    def best_around(self, point):
-        best, tools = self._best[point]
-        bested = set()
-        for tool in tools:
-            for p in point.around:
-                if p.y >= self.height or p.x >= self.width:
-                    continue
-                tools_ok = self.tool_ok(p)
-                if tool in tools_ok:
-                    next_best = best + 1
-                    next_tools = [tool]
-                else:
-                    next_best = best + 8
-                    next_tools = tools_ok
-                for p_tool in next_tools:
-                    if p not in self._best or self._best[p][0] > next_best:
-                        self._best[p] = (next_best, [p_tool])
-                        if p.y < point.y or p.x < point.x:
-                            self.best_around(p)
-                    elif self._best[p][0] == next_best and p_tool not in self._best[p][1]:
-                        self._best[p][1].append(p_tool)
-                        if p.y < point.y or p.x < point.x:
-                            self.best_around(p)
-        return bested
+    """
+            1  function Dijkstra(Graph, source):
+            2      dist[source] ← 0                           // Initialization
+            3
+            4      create vertex set Q
+            5
+            6      for each vertex v in Graph:
+            7          if v ≠ source
+            8              dist[v] ← INFINITY                 // Unknown distance from source to v
+            9          prev[v] ← UNDEFINED                    // Predecessor of v
+            10
+            11         Q.add_with_priority(v, dist[v])
+            12
+            13
+            14     while Q is not empty:                      // The main loop
+            15         u ← Q.extract_min()                    // Remove and return best vertex
+            16         for each neighbor v of u:              // only v that are still in Q
+            17             alt ← dist[u] + length(u, v)
+            18             if alt < dist[v]
+            19                 dist[v] ← alt
+            20                 prev[v] ← u
+            21                 Q.decrease_priority(v, alt)
+            22
+            23     return dist, prev
+"""
 
-    def explore(self):
-        self._best = {self.origin: (0, [TORCH])}
+    def dijkstra(self):
+        origin = PointTool(self.origin, TORCH)
+        target = PointTool(self.target, TORCH)
+
+        vertices = set([origin, target])
         for y in range(self.height):
             for x in range(self.width):
                 point = Point(x, y)
-                self.best_around(point)
-        return self._best[self.target]
+                if point != origin.point and point != target.point:
+                    for tool in self.tool_ok(point):
+                        vertices.add(PointTool(point, tool))
 
+        dist = {origin: 0}
+        prev = {}
+        heapq = [(0, origin)]
 
-"""
-1  function Dijkstra(Graph, source):
-2      dist[source] ← 0                           // Initialization
-3
-4      create vertex set Q
-5
-6      for each vertex v in Graph:
-7          if v ≠ source
-8              dist[v] ← INFINITY                 // Unknown distance from source to v
-9          prev[v] ← UNDEFINED                    // Predecessor of v
-10
-11         Q.add_with_priority(v, dist[v])
-12
-13
-14     while Q is not empty:                      // The main loop
-15         u ← Q.extract_min()                    // Remove and return best vertex
-16         for each neighbor v of u:              // only v that are still in Q
-17             alt ← dist[u] + length(u, v)
-18             if alt < dist[v]
-19                 dist[v] ← alt
-20                 prev[v] ← u
-21                 Q.decrease_priority(v, alt)
-22
-23     return dist, prev
-"""
+        for point in vertices:
+            if point != origin:
+                dist[point] = float("inf")
+            prev[point] = None
+            heappush(heapq, (dist[point], point))
+
+        while heapq:
+            minutes, current = heappop(heapq)
+            tool_ok = self.tool_ok(current.point)
+            for p in current.point.around:
+                if p.y >= self.height or p.x >= self.width or p == origin.point:
+                    continue
+                for tool in self.tool_ok(p):
+                    if tool in tool_ok:
+                        next_minutes = minutes + 1
+                        if tool != current.tool:
+                            next_minutes += 7
+                        next_p = PointTool(p, tool)
+                        if next_p not in dist:
+                            continue
+                        if next_minutes < dist[next_p]:
+                            dist[next_p] = next_minutes
+                            prev[next_p] = current
+                            heappush(heapq, (next_minutes, PointTool(p, tool)))
+        return dist[target]
 
 
 cave = Cave(510, Point(10, 10))
-print(cave.risk)
-print(cave.explore())
+# print(cave.risk)
+# print(cave.dijkstra())
 
 # depth: 5616
 # target: 10,785
 cave = Cave(5616, Point(10, 785))
 print(cave.risk)
-print(cave.explore())
-#1062
+print(cave.dijkstra())
