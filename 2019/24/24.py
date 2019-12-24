934cfa9fdf8a56353c07febb57b2a58403b2ed8a
Marco Beri <marcoberi@gmail.com>
2019-12-24 18:04:44 +0000

    24.py edited online with Bitbucket

--- 2019/24/24.py
+++ 2019/24/24.py
@@ -1,3 +1,6 @@
+from functools import lru_cache
+
+
 class Life:
     def __init__(self, raw, size=5):
         self.map = [row for row in raw.split("\n")]
@@ -5,19 +8,16 @@ class Life:
 
     def around(self, x, y):
         count = 0
-        for y2 in (y - 1, y, y + 1):
+        for x2, y2 in ((x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)):
             if y2 >= 0 and y2 < self.size:
-                for x2 in (x - 1, x, x + 1):
-                    if x2 >= 0 and x2 < self.size:
-                        if y != y2 and x != x2:
-                            if self.map[y2][x2] == '#':
-                                count += 1
+                if x2 >= 0 and x2 < self.size:
+                    if self.map[y2][x2] == '#':
+                        count += 1
         return count
 
     def evolve(self):
         states = set()
         while True:
-            print(self.map)
             if self.state in states:
                 return
             states.add(self.state)
@@ -51,12 +51,62 @@ class Life:
         return bio
 
 
+class LifeRec:
+    def __init__(self, raw, size=5):
+        self.size = size
+        data = raw.split("\n")
+        self.bugs = set((x, y, 0) for x in range(5) for y in range(5) if data[y][x] == "#")
+    
+    @lru_cache(maxsize=None)
+    def around(self, x, y, depth):
+        cells = []
+        for x2, y2 in ((x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)):
+            if y2 >= 0 and y2 < self.size:
+                if x2 >= 0 and x2 < self.size:
+                    if x2 != 2 or y2 != 2:
+                        cells.append((x2, y2, depth))
+                    else:
+                        if x == 2:
+                            for x3 in range(5):
+                                cells.append((x3, 0 if y == 1 else self.size - 1, depth + 1))
+                        else:
+                            assert y == 2
+                            for y3 in range(5):
+                                cells.append((0 if x == 1 else self.size - 1, y3, depth + 1))
+                else:
+                    cells.append((1 if x2 < 0 else 3, 2, depth - 1))
+            else:
+                cells.append((2, 1 if y2 < 0 else 3, depth - 1))
+        return cells
+
+    def evolve(self, steps):
+        for step in range(steps):
+            bugs = set()
+            seen = set()
+            for bug in self.bugs:
+                seen.add(bug)
+                count = sum(1 for cell in self.around(*bug) if cell in self.bugs)
+                if count == 1:
+                    bugs.add(bug)
+                for cell in self.around(*bug):
+                    if cell not in seen and cell not in self.bugs:
+                        seen.add(cell)
+                        count = sum(1 for cell2 in self.around(*cell) if cell2 in self.bugs)
+                        if count in (1, 2):
+                            bugs.add(cell)
+                	
+            self.bugs = bugs
+            
+            
 if __name__ == "__main__":
-    life = Life(open("input.txt").read().strip())
-    life = Life("""....#
-#..#.
-#..##
-..#..
-#....""")
+    raw = """#....
+#...#
+##.##
+....#
+#.##."""
+    life = Life(raw)
     life.evolve()
     print(life.biodiversity)
+    life = LifeRec(raw)
+    life.evolve(200)
+    print(len(life.bugs))
