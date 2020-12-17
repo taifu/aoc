3739657e7aa6278ba657ada45bb4b61ca884fc16
Marco Beri <marcoberi@gmail.com>
2020-12-17 15:45:20 +0100

    Improved and generalized

--- 2020/17/day_17.py
+++ 2020/17/day_17.py
@@ -1,34 +1,33 @@
-def parse(data, four_dimensions):
+from itertools import product
+
+
+def parse(data, dimensions):
     space = {}
     for row, line in enumerate(data.strip().split('\n')):
         for col, pos in enumerate(list(line)):
             if pos == '#':
-                space[(row, col, 0) + ((0,) if four_dimensions else ())] = 1
+                space[(row, col) + (0,) * (dimensions - 2)] = 1
     return space
 
 
-def around(point, four_dimensions, this_included=False):
-    for dx in range(-1, 2):
-        for dy in range(-1, 2):
-            for dz in range(-1, 2):
-                if four_dimensions:
-                    for dw in range(-1, 2):
-                        if this_included or not (dx == 0 and dy == 0 and dz == 0 and dw == 0):
-                            yield point[0] + dx, point[1] + dy, point[2] + dz, point[3] + dw
-                else:
-                    if this_included or not (dx == 0 and dy == 0 and dz == 0):
-                        yield point[0] + dx, point[1] + dy, point[2] + dz
+def around(point, dimensions):
+    for deltas in product(range(-1, 2), repeat=dimensions):
+        yield tuple(coord + delta for coord, delta in zip(point, deltas))
 
 
-def solve(data, four_dimensions=False):
-    space = parse(data, four_dimensions)
-    for cont in range(6):
-        new_space = {}
-        for point, current in space.items():
-            for new_point in around(point, four_dimensions, this_included=True):
-                active = sum(space.get(around_point, 0) for around_point in around(new_point, four_dimensions))
-                if space.get(new_point, 0) == 1 and active in (2, 3) or active == 3:
-                    new_space[new_point] = 1
+def solve(data, dimensions=3, cycle=6):
+    space = parse(data, dimensions)
+    for cont in range(cycle):
+        new_space, visited = {}, set()
+        for point in space.keys():
+            for new in around(point, dimensions):
+                if new in visited:
+                    continue
+                visited.add(new)
+                current = space.get(new, 0)
+                active = sum(space.get(neigh, 0) for neigh in around(new, dimensions)) - current
+                if current == 1 and active in (2, 3) or active == 3:
+                    new_space[new] = 1
         space = new_space
     return sum(space.values())
 
@@ -36,4 +35,4 @@ def solve(data, four_dimensions=False):
 if __name__ == "__main__":
     data = open("input.txt").read()
     print(solve(data))
-    print(solve(data, True))
+    print(solve(data, 4))
