8c81b88e68e491114b9a9e4ff3a88b2928ce3f1d
Marco Beri <marcoberi@gmail.com>
2020-12-18 06:50:52 +0100

    Day 18

--- 2020/18/day_18.py
+++ 2020/18/day_18.py
@@ -1,17 +1,49 @@
-def parse(data, dimensions):
-    space = set()
-    for row, line in enumerate(data.strip().split('\n')):
-        for col, pos in enumerate(list(line)):
-            if pos == '#':
-                space.add((col, row) + (0,) * (dimensions - 2))
-    return space
+from operator import add, mul
 
 
-def solve1(data):
-    pass
+def parse(data):
+    exprs = []
+    for line in data.strip().split('\n'):
+        line = line.replace("(", "( ").replace(")", " )")
+        exprs.append(list(int(x) if x.isdigit() else (add if x == '+' else mul if x == '*' else x) for x in line.split(" ")))
+    return exprs
+
+
+def find_closing(expr, pos):
+    opened = 1
+    while opened > 0:
+        pos += 1
+        if expr[pos] == ")":
+            opened -= 1
+        elif expr[pos] == "(":
+            opened += 1
+    return pos
+
+
+def evaluate(expr, precedence):
+    while "(" in expr:
+        pos = expr.index("(")
+        closing = find_closing(expr, pos)
+        expr[pos:closing + 1] = [evaluate(expr[pos + 1:closing], precedence)]
+    while precedence in expr:
+        pos = expr.index(precedence)
+        expr[pos - 1:pos + 2] = [precedence(expr[pos - 1], expr[pos + 1])]
+    tot = expr.pop(0)
+    while len(expr) > 1:
+        tot = expr.pop(0)(tot, expr.pop(0))
+    return tot
+
+
+def solve(data, precedence=None):
+    exprs = parse(data)
+    tot = 0
+    for expr in exprs:
+        tot += evaluate(expr, precedence)
+    return tot
 
 
 if __name__ == "__main__":
     data = open("input.txt").read()
-    print(solve1(data))
-    #print(solve2(data))
+    print(solve(data))
+    print(solve(data, precedence=add))
+    print(solve(data, precedence=mul))
--- 2020/18/test_18.py
+++ 2020/18/test_18.py
@@ -1,18 +1,40 @@
 import unittest
-import pytest
+from operator import add, mul
 
 import day_18 as day
 
 
 class TestDay(unittest.TestCase):
-    def setUp(self):
-        self.data = """.#.
-..#
-###
-"""
+    def test_part1_1(self):
+        self.assertEqual(day.solve("2 * 3 + (4 * 5)"), 26)
+
+    def test_part1_2(self):
+        self.assertEqual(day.solve("5 + (8 * 3 + 9 + 3 * 4 * 3)"), 437)
+
+    def test_part1_3(self):
+        self.assertEqual(day.solve("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"), 12240)
+
+    def test_part1_4(self):
+        self.assertEqual(day.solve("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"), 13632)
+
+    def test_part2_1(self):
+        self.assertEqual(day.solve("1 + (2 * 3) + (4 * (5 + 6))", add), 51)
+
+    def test_part2_2(self):
+        self.assertEqual(day.solve("2 * 3 + (4 * 5)", add), 46)
+
+    def test_part2_3(self):
+        self.assertEqual(day.solve("5 + (8 * 3 + 9 + 3 * 4 * 3)", add), 1445)
+
+    def test_part2_4(self):
+        self.assertEqual(day.solve("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", add), 669060)
+
+    def test_part2_5(self):
+        self.assertEqual(day.solve("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", add), 23340)
 
     def test_solution(self):
         import os
-        #data = open(os.path.dirname(__file__) + "/input.txt").read()
-        #self.assertEqual(day.solve(data), 265)
-        #self.assertEqual(day.solve(data, 4), 1936)
+        data = open(os.path.dirname(__file__) + "/input.txt").read()
+        self.assertEqual(day.solve(data), 701339185745)
+        self.assertEqual(day.solve(data, add), 4208490449905)
+        self.assertEqual(day.solve(data, mul), 12312398996)
