fcdf8a3a2eae02db5fb4c2df62553241a410f6f5
Marco Beri <marcoberi@gmail.com>
2021-12-17 21:11:31 +0100

    Cached day 17

--- 2021/17/day_17.py
+++ 2021/17/day_17.py
@@ -1,4 +1,6 @@
 class Trench:
+    cache = {}
+
     def __init__(self, data):
         target_x = tuple(int(p) for p in data.split("x=")[1].split(",")[0].split(".."))
         target_y = tuple(int(p) for p in data.split("y=")[-1].split(".."))
@@ -8,17 +10,19 @@ class Trench:
         x, y, max_y = 0, 0, 0
         while True:
             if x > self.target[0][1] or -y > -self.target[1][0]:
-                max_y = None
-                break
+                return None
             if x >= self.target[0][0] and -y >= -self.target[1][1]:
-                break
+                return max_y
             x, vel_x = x + vel_x, max(0, vel_x - 1)
             y, vel_y = y + vel_y, vel_y - 1
             if y > max_y:
                 max_y = y
-        return max_y
 
     def highest(self):
+        try:
+            return Trench.cache[self.target]
+        except KeyError:
+            pass
         max_max_y, max_val, cont = 0, 200, 0
         for vel_x in range(1, max_val):
             for vel_y in range(-max_val, max_val):
@@ -26,6 +30,7 @@ class Trench:
                     cont += 1
                 if max_y is not None and max_y > max_max_y:
                     max_max_y = max_y
+        Trench.cache[self.target] = max_max_y, cont
         return max_max_y, cont
 
 
