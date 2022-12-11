374835e00341d846d2525c8400d7b9fd47325e6a
Marco Beri <marcoberi@gmail.com>
2022-12-11 06:44:28 +0100

    Day 11

---
+++ 2022/11/day_11.py
@@ -0,0 +1,59 @@
+from operator import mul, add
+from functools import reduce
+
+
+class Monkey:
+    def _operation(self, line):
+        parts = line.split()
+        operator = {"*": mul, "+":add}[parts[1]]
+        if parts[0] == parts[2] == "old":
+            return lambda old: operator(old, old)
+        elif parts[0] == "old":
+            return lambda old: operator(old, int(parts[2]))
+        elif parts[2] == "old":
+            return lambda old: operator(int(parts[0]), old)
+        else:
+            return lambda old: operator(int(parts[0]), int(parts[2]))
+
+    def __init__(self, lines):
+        self.inspected = 0
+        self.number = int(lines[0].split()[1][:-1])
+        self.items = [int(item.strip()) for item in lines[1].split(":")[1].split(",")]
+        self.operation = self._operation(lines[2].split("=")[1].strip())
+        self.divisible = int(lines[3].split(" ")[-1])
+        self.results = [int(line.split()[-1]) for line in lines[5:3:-1]]
+
+    def turn(self, monkeys, divided, divisibility):
+        while self.items:
+            self.inspected += 1
+            item = (self.operation(self.items.pop()) // divided) % divisibility
+            monkeys[self.results[item % self.divisible == 0]].items.append(item)
+
+
+def load(data):
+    monkeys = []
+    for lines in data.strip().split("\n\n"):
+        monkeys.append(Monkey(lines.split("\n")))
+    return monkeys
+
+
+def compute(monkeys, turns=20, divided=3):
+    divisibility = reduce(mul, [m.divisible for m in monkeys])
+    for turn in range(turns):
+        for monkey in monkeys:
+            monkey.turn(monkeys, divided, divisibility)
+    return mul(*[m.inspected for m in sorted(monkeys, key=lambda m: m.inspected)[-2:]])
+
+
+def solve1(data):
+    return compute(load(data))
+
+
+def solve2(data):
+    return compute(load(data), 10000, 1)
+
+
+if __name__ == "__main__":
+    data = open("input.txt").read()
+    print(solve1(data))
+    print(solve2(data))
---
+++ 2022/11/test_11.py
@@ -0,0 +1,47 @@
+import unittest
+day = __import__('day_' + __file__[-5:-3])
+
+
+class TestDay(unittest.TestCase):
+    def setUp(self):
+        self.data = """Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+"""
+
+    def test_part_1(self):
+        self.assertEqual(10605, day.solve1(self.data))
+
+    def test_part_2(self):
+        self.assertEqual(2713310158, day.solve2(self.data))
+
+    def test_solution(self):
+        import os
+        with open(os.path.dirname(__file__) + "/input.txt") as f_data:
+            data = f_data.read()
+            self.assertEqual(day.solve1(data), 90294)
+            self.assertEqual(day.solve2(data), 18170818354)
