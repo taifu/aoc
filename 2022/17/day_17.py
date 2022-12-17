8112acb823bc2d8214795f69df6c81211c27232f
Marco Beri <marcoberi@gmail.com>
2022-12-17 11:06:26 +0100

    Day 17

--- 2022/17/day_17.py
+++ 2022/17/day_17.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def load(data):
     return [{">": 1, "<": -1}[char] for char in data.strip()]
 
@@ -7,42 +10,69 @@ class Grid:
         self.n_rocks = 0
         self.grid = set()
         self.width = width
-        self.current_rock = 0
-        self.height = -1j
+        self.height = 3j
+        self.tops = [-1] * self.width
+        self.cache = defaultdict(list)
+        self.delta = self.delta_height = None
+        self.deltas = set()
+        self.rocks = [(0, 1, 2, 3),
+                      (1, 1j, 1 + 1j, 2 + 1j, 1 + 2j),
+                      (0, 1, 2, 2 + 1j, 2 + 2j),
+                      (0, 1j, 2j, 3j),
+                      (0, 1, 1j, 1 + 1j),
+                      ]
         self.add_rock()
 
     def add_rock(self):
-        rocks = [(0, 1, 2, 3),
-                 (1, 1j, 1 + 1j, 2 + 1j, 1 + 2j),
-                 (0, 1, 2, 2 + 1j, 2 + 2j),
-                 (0, 1j, 2j, 3j),
-                 (0, 1, 1j, 1 + 1j),
-                 ]
-        self.current_pos = 2 + (self.height.imag + 4) * 1j
-        self.rock = tuple(xy + self.current_pos for xy in rocks[self.current_rock % 5])
-        self.current_rock += 1
         self.n_rocks += 1
+        self.current_pos = 2 + self.height.imag * 1j
+        self.rock = tuple(xy + self.current_pos for xy in self.rocks[(self.n_rocks - 1) % len(self.rocks)])
 
-    def draw(self):
+    def draw(self, first=False):
+        if first:
+            print("\033[2J")
         print("\033[0;0H")
-        for y in range(int(self.height.imag) + 6, -1, -1):
-            line = ""
-            for x in range(self.width):
-                dot = x + y * 1j
-                if dot in self.grid:
-                    line += "#"
-                elif dot in self.rock:
-                    line += "@"
-                else:
-                    line += "."
-            print(f"|{line}|")
-        print(f"+-------+")
+        top = int(self.height.imag) + 2
+        rang = range(top, max(-1, top - 20), -1)
+        while True:
+            for y in rang:
+                line = ""
+                for x in range(self.width):
+                    dot = x + y * 1j
+                    if dot in self.grid:
+                        line += "#"
+                    elif dot in self.rock:
+                        line += "@"
+                    else:
+                        line += "."
+                print(f"|{line}| {y}")
+            if y == 0:
+                print(f"+-------+")
+                break
+            rang = range(min(y - 1, 20), -1, -1)
+            print()
 
-    def resting(self):
+    def caching(self, moves):
+        key = (self.n_rocks % len(self.rocks), moves, tuple(self.height.imag - t for t in self.tops))
+        self.cache[key].append((self.n_rocks, self.height))
+        if len(self.cache[key]) > 5:
+            delta = self.cache[key][-1][0] - self.cache[key][-2][0]
+            self.deltas.add(delta)
+            if self.delta is None:
+                self.delta = delta
+                self.delta_height = self.cache[key][-1][1] - self.cache[key][-2][1]
+                assert self.delta % len(self.rocks) == 0
+        return
+
+    def resting(self, moves):
         for xy in self.rock:
             self.grid.add(xy)
-            if xy.imag > self.height.imag:
-                self.height = xy
+            if xy.imag + 4 > self.height.imag:
+                self.height = xy + 4j
+            if xy.imag > self.tops[int(xy.real)]:
+                self.tops[int(xy.real)] = xy.imag
+        self.caching(moves)
+        self.add_rock()
 
     def move(self, move):
         next_rock = tuple(xy + move for xy in self.rock)
@@ -54,32 +84,42 @@ class Grid:
 
 
 class Tetris:
-    def __init__(self,gases, width=7):
+    def __init__(self,gases, width=7, drawing=False):
         self.gases = gases
         self.width = width
+        self.drawing = drawing
 
     def height(self, n_rocks=2022):
         grid = Grid(self.width)
-        #grid.draw()
+        if self.drawing:
+            grid.draw(first=True)
         moves = 0
-        while grid.n_rocks < n_rocks:
+        delta_height = delta_rocks = 0
+        while grid.n_rocks + delta_rocks < n_rocks:
             # left/right
-            grid.move(self.gases[moves % len(self.gases)])
-            moves += 1
-            #grid.draw()
+            grid.move(self.gases[moves])
+            moves = (moves + 1) % len(self.gases)
             # down
             if not grid.move(-1j):
-                grid.resting()
-                grid.add_rock()
-            #grid.draw()
-        return int(grid.height.imag) + 4
+                grid.resting(moves)
+                if grid.delta and delta_height == 0:
+                    steps = (n_rocks - grid.n_rocks) // grid.delta
+                    if steps:
+                        delta_rocks = steps * grid.delta
+                        delta_height = int(steps * grid.delta_height.imag)
+                        pass
+
+            if self.drawing:
+                grid.draw()
+        return int(grid.height.imag) + delta_height
 
 
-def solve1(data):
-    return Tetris(load(data)).height()
+def solve1(data, drawing=False):
+    return Tetris(load(data), drawing=drawing).height()
 
 
 def solve2(data):
+    return Tetris(load(data)).height(n_rocks=1000000000000)
     mostest = []
     for elves, items in load(data).items():
         mostest.append(sum(items))
@@ -87,6 +127,7 @@ def solve2(data):
 
 
 if __name__ == "__main__":
+    import sys
     data = open("input.txt").read()
-    print(solve1(data))
+    print(solve1(data, drawing="-d" in sys.argv))
     print(solve2(data))
--- 2022/17/test_17.py
+++ 2022/17/test_17.py
@@ -9,11 +9,11 @@ class TestDay(unittest.TestCase):
     def test_part_1(self):
         self.assertEqual(3068, day.solve1(self.data))
 
-    def _test_part_2(self):
-        self.assertEqual(45000, day.solve2(self.data))
+    def test_part_2(self):
+        self.assertEqual(1514285714288, day.solve2(self.data))
 
     def test_solution(self):
         import os
         data = open(os.path.dirname(__file__) + "/input.txt").read()
         self.assertEqual(3111, day.solve1(data))
-        #self.assertEqual(201491, day.solve2(data))
+        self.assertEqual(1526744186042, day.solve2(data))
