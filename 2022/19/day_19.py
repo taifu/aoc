3302e4aa7e5a32e098e4dc1ffbec497e8d10111a
Marco Beri <marcoberi@gmail.com>
2022-12-19 08:33:12 +0100

    Pruning to do: not even the test finish

--- 2022/19/day_19.py
+++ 2022/19/day_19.py
@@ -10,27 +10,49 @@ class Blueprint:
         self.robot_costs = [[data[0], 0, 0, 0], [data[1], 0, 0, 0], [data[2], data[3], 0, 0], [data[4], 0, data[5], 0]]
 
     def best(self, minutes):
-        run = deque(((0, [1, 0, 0, 0], [0, 0, 0, 0]),))
-        best, seen = 0, {}
+        run = deque(((0, (1, 0, 0, 0), (0, 0, 0, 0)),))
+        best, seen = 0, set()
         while run:
             minute, robots, minerals = run.popleft()
-            print(len(run), minute)
             minute += 1
             buildable_robots = []
             for robot in range(4):
                 if all(self.robot_costs[robot][m] <= minerals[m] for m in range(4)):
                     buildable_robots.append(robot)
-            collected_minerals = [minerals[n] + robots[n] for n in range(4)]
+            # Debug
+            #print(f"{'  '*minute} Entering {minute} robots={robots} minerals={minerals} buildable_robots={buildable_robots}")
             if minute == minutes:
-                if collected_minerals[3] > best:
-                    best = collected_minerals[3]
+                if minerals[3] > best:
+                    best = minerals[3]
             else:
-                run.append((minute, robots, collected_minerals))
-                for robot in buildable_robots:
-                    next_robots = robots.copy()
-                    next_robots[robot] += 1
-                    run.append((minute, next_robots, [minerals[n] - self.robot_costs[robot][n] for n in range(4)]))
-        #import pdb; pdb.set_trace()
+                if buildable_robots:
+                    for robot in buildable_robots:
+                        next_robots = tuple(robots[n] + (1 if n == robot else 0) for n in range(4))
+                        next_minerals = tuple(minerals[n] - self.robot_costs[robot][n] + robots[n] for n in range(4))
+                        if not (minute, next_robots, next_minerals) in seen:
+                            seen.add((minute, next_robots, next_minerals))
+                            if minute == 7 and next_robots == (1, 3, 0, 0) and next_minerals == (1, 6, 0, 0) :
+                                print(f"Found {minute} robots={next_robots} minerals={next_minerals}")
+                            elif minute == 11 and next_robots == (1, 3, 1, 0) and next_minerals == (2, 4, 0, 0) :
+                                print(f"Found {minute} robots={next_robots} minerals={next_minerals}")
+                            elif minute == 12 and next_robots == (1, 4, 1, 0) and next_minerals == (1, 7, 1, 0) :
+                                print(f"Found {minute} robots={next_robots} minerals={next_minerals}")
+                            elif minute == 15 and next_robots == (1, 4, 2, 0) and next_minerals == (1, 5, 4, 0) :
+                                print(f"Found {minute} robots={next_robots} minerals={next_minerals}")
+                            elif minute == 18 and next_robots == (1, 4, 2, 1) and next_minerals == (2, 17, 3, 0) :
+                                print(f"Found {minute} robots={next_robots} minerals={next_minerals}")
+                            elif minute == 21 and next_robots == (1, 4, 2, 2) and next_minerals == (3, 29, 2, 3) :
+                                print(f"Found {minute} robots={next_robots} minerals={next_minerals}")
+                            elif minute == 23 and next_robots == (1, 4, 2, 2) and next_minerals == (6, 41, 8, 9) :
+                                print(f"Found {minute} robots={next_robots} minerals={next_minerals}")
+                            #print(f"{'  '*minute}     Exit {minute} robots={next_robots} minerals={next_minerals}")
+                            run.append((minute, next_robots, next_minerals))
+                minerals = tuple(minerals[n] + robots[n] for n in range(4))
+                if not (minute, robots, minerals) in seen:
+                    seen.add((minute, robots, minerals))
+                    #print(f"{'  '*minute}     Exit {minute} robots={robots} minerals={minerals}")
+                    run.append((minute, robots, minerals))
+        print(f"Best {best}")
         return best
 
 
--- 2022/19/test_19.py
+++ 2022/19/test_19.py
@@ -4,28 +4,17 @@ day = __import__('day_' + __file__[-5:-3])
 
 class TestDay(unittest.TestCase):
     def setUp(self):
-        self.data = """2,2,2
-1,2,2
-3,2,2
-2,1,2
-2,3,2
-2,2,1
-2,2,3
-2,2,4
-2,2,6
-1,2,5
-3,2,5
-2,1,5
-2,3,5
+        self.data = """Blueprint 1: Each ore robot costs 4 ore.  Each clay robot costs 2 ore.  Each obsidian robot costs 3 ore and 14 clay.  Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore.  Each clay robot costs 3 ore.  Each obsidian robot costs 3 ore and 8 clay.  Each geode robot costs 3 ore and 12 obsidian.
 """
 
     def test_part_1(self):
-        self.assertEqual(64, day.solve1(self.data))
+        self.assertEqual(33, day.solve1(self.data))
 
-    def test_part_2(self):
+    def _test_part_2(self):
         self.assertEqual(58, day.solve2(self.data))
 
-    def test_solution(self):
+    def _test_solution(self):
         import os
         data = open(os.path.dirname(__file__) + "/input.txt").read()
         self.assertEqual(4400, day.solve1(data))
