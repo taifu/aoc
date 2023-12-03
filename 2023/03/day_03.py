16a38788a67bf313c4207cb8737b82b51d76c98a
Marco Beri <marcoberi@gmail.com>
2023-12-03 16:49:28 +0100

    Datatype hints

--- 2023/03/day_03.py
+++ 2023/03/day_03.py
@@ -1,10 +1,17 @@
-class Schema:
-    def __init__(self, data):
-        self.map = [list(line) for line in data.strip().split('\n')]
-        self.height = len(self.map)
-        self.width = len(self.map[0])
+from dataclasses import dataclass
+from typing import Generator
 
-    def numbers(self):
+
+@dataclass
+class Schema:
+    map: list[list]
+    height: int
+    width: int
+
+    NUMBER = Generator[tuple[int, int, int, int], None, None]
+    AROUND = Generator[tuple[str, int, int], None, None]
+
+    def numbers(self) -> NUMBER:
         for y in range(self.height):
             x = 0
             while x < self.width:
@@ -20,21 +27,21 @@ class Schema:
                 else:
                     x += 1
 
-    def around(self, x, y, length):
+    def around(self, x: int, y: int, length: int) -> AROUND:
         for d_y in (-1, 0, 1):
             if 0 <= y + d_y < self.height:
                 for d_x in range(-1, length + 1, 1 if d_y != 0 else length + 1):
                     if 0 <= x + d_x < self.width:
                         yield self.map[y + d_y][x + d_x], x + d_x, y + d_y
 
-    def sum(self):
+    def sum(self) -> int:
         tot = 0
         for length, x, y, number in self.numbers():
-            if any(cell != '.' for cell, dx, dy in self.around(x, y, length)):
+            if any(cell != "." for cell, dx, dy in self.around(x, y, length)):
                 tot += number
         return tot
 
-    def gears(self):
+    def gears(self) -> int:
         tot = 0
         all_numbers = list(self.numbers())
         for n1, (l1, x1, y1, number1) in enumerate(all_numbers[:-1]):
@@ -44,17 +51,23 @@ class Schema:
                 if x2 + l2 > x1 - 2 and x2 < x1 + l1 + 2:
                     for cell, dx1, dy1 in self.around(x1, y1, l1):
                         for _, dx2, dy2 in self.around(x2, y2, l2):
-                            if dx1 == dx2 and dy1 == dy2 and cell == '*':
+                            if dx1 == dx2 and dy1 == dy2 and cell == "*":
                                 tot += number1 * number2
         return tot
 
 
-def solve1(data):
-    return Schema(data).sum()
+def load(data: str) -> Schema:
+    map = [list(line) for line in data.strip().split("\n")]
+    height, width = len(map), len(map[0])
+    return Schema(map, height, width)
 
 
-def solve2(data):
-    return Schema(data).gears()
+def solve1(data: str) -> int:
+    return load(data).sum()
+
+
+def solve2(data: str) -> int:
+    return load(data).gears()
 
 
 if __name__ == "__main__":
