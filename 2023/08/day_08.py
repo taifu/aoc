e8dcc3c4fea367ec01db17978e853bbe8f32b875
Marco Beri <marcoberi@gmail.com>
2023-12-08 06:56:10 +0100

    27 lines under 80 chars :-)

--- 2023/08/day_08.py
+++ 2023/08/day_08.py
@@ -5,23 +5,21 @@ class Map:
     def __init__(self, data: str):
         lines = data.splitlines()
         self.graph: dict[str, dict[str, str]] = {}
-        self.directions: list[str] = list(lines[0])
+        self.LRs: list[str] = list(lines[0])
         for line in lines[2:]:
             self.graph[line[0:3]] = {'L': line[7:10], 'R': line[12:15]}
 
     def steps(self, start_from: str = 'AAA', ends: list[str] = ['ZZZ']) -> int:
         count, start = 0, start_from
         while start not in ends:
-            start = self.graph[start][self.directions[count % len(self.directions)]]
+            start = self.graph[start][self.LRs[count % len(self.LRs)]]
             count += 1
         return count
 
     def steps_all(self) -> int:
-        counts, starts = [], [start for start in self.graph.keys() if start[-1] == 'A']
+        starts = [start for start in self.graph.keys() if start[-1] == 'A']
         ends: list[str] = [end for end in self.graph.keys() if end[-1] == 'Z']
-        for start in starts:
-            counts.append(self.steps(start, ends))
-        return lcm(*counts)
+        return lcm(*[self.steps(start, ends) for start in starts])
 
 
 def solve1(data: str) -> int:
