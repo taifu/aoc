f058f2db08b0a1d60c6191b9ee34c722a22f4bbc
Marco Beri <marcoberi@gmail.com>
2023-12-18 15:58:51 +0100

    Day 18

---
+++ 2023/18/day_18.py
@@ -0,0 +1,65 @@
+class Plan2:
+    def __init__(self, raw: str, part2=False):
+        self.vertical_walls = []
+        self.size = [-10e100, 10e100, -10e100, 10e100]
+        dxy = {'3': (0, -1), '2': (-1, 0), '1': (0, 1), '0': (1, 0)}
+        ys = set((0,))
+        self.length = 0
+        last_pos = [0, 0]
+        for line in raw.splitlines():
+            pos = last_pos[:]
+            if part2:
+                value = line.split(' ')[2][2:-1]
+                length = int(value[:-1], 16)
+                dxy_char = value[-1]
+            else:
+                parts = line.split(' ')
+                length = int(parts[1])
+                dxy_char = {'R': '0', 'D': '1', 'L': '2', 'U': '3'}[parts[0][0]]
+            backward = dxy_char in '23'
+            vertical = dxy_char in '31'
+            self.length += length
+            for n in range(2):
+                pos[n] += dxy[dxy_char][n] * length
+            wall = (pos + last_pos) if backward else (last_pos + pos)
+            ys.add(pos[1])
+            if vertical:
+                self.vertical_walls.append(wall)
+            for n, xy in enumerate(pos):
+                for m, fun in enumerate((max, min)):
+                    self.size[n * 2 + m] = fun(self.size[n * 2 + m], xy)
+            last_pos = pos
+        self.ys = sorted(ys)
+
+    def vertical_intercepts(self, y0, y1):
+        return sorted(set([x for wall in self.vertical_walls
+                           if wall[1] < y1 and wall[3] > y0 for x in (wall[0], wall[2])]))
+
+    def area(self):
+        area = 0
+        for y0_slice, y1_slice in zip(self.ys, self.ys[1:]):
+            x_slices = self.vertical_intercepts(y0_slice, y1_slice)
+            pair = 0
+            if self.size[0] not in x_slices:
+                pair = 1
+                x_slices.insert(0, self.size[0])
+            if self.size[2] not in x_slices:
+                x_slices.append(self.size[2])
+            for x0, x1 in zip(x_slices[pair::2], x_slices[pair + 1::2]):
+                area += (y1_slice - y0_slice) * (x1 - x0)
+        return area + self.length // 2 + 1
+
+
+def solve1(data: str) -> int:
+    return Plan2(data).area()
+
+
+def solve2(data: str) -> int:
+    return Plan2(data, True).area()
+
+
+if __name__ == "__main__":
+    import sys
+    data = open((sys.argv + ["input.txt"])[1]).read()
+    print(solve1(data))
+    #print(solve2(data))
---
+++ 2023/18/test_18.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+import pytest  # noqa: F401
+day = __import__('day_' + __file__[-5:-3])
+
+
+class TestDay(unittest.TestCase):
+    def setUp(self) -> None:
+        self.data = open(os.path.dirname(__file__) + "/input.txt").read()
+        self.data_example = open(os.path.dirname(__file__) + "/example.txt").read()
+        self.data_example_2 = open(os.path.dirname(__file__) + "/example2.txt").read()
+
+    def test_part_1(self) -> None:
+        self.assertEqual(62, day.solve1(self.data_example))
+
+    def test_part_1_2(self) -> None:
+        self.assertEqual(77, day.solve1(self.data_example_2))
+
+    def test_part_2(self) -> None:
+        self.assertEqual(952408144115, day.solve2(self.data_example))
+
+    def test_solution_1(self) -> None:
+        self.assertEqual(day.solve1(self.data), 39194)
+
+    def test_solution_2(self) -> None:
+        self.assertEqual(day.solve2(self.data), 78242031808225)
