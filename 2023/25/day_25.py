1e04e549be8050da8c15bee81ddbf320ea17f255
Marco Beri <marcoberi@gmail.com>
2023-12-25 17:29:33 +0100

    Optimized

--- 2023/25/day_25.py
+++ 2023/25/day_25.py
@@ -1,11 +1,11 @@
-from collections import deque, defaultdict
+from collections import deque, defaultdict, OrderedDict
 from itertools import combinations
 from operator import itemgetter
 
 
 class Wires:
     def __init__(self, raw: str) -> None:
-        self.graph = defaultdict(set)
+        self.graph: OrderedDict[str, list[str]] = OrderedDict()
         self.nodes = set()
 
         for line in raw.splitlines():
@@ -14,16 +14,23 @@ class Wires:
             self.nodes.add(wire_from)
             for wire_to in wires[1:]:
                 self.nodes.add(wire_to)
-                self.graph[wire_from].add(wire_to)
-                self.graph[wire_to].add(wire_from)
+                try:
+                    self.graph[wire_from].append(wire_to)
+                except KeyError:
+                    self.graph[wire_from] = [wire_to]
+                try:
+                    self.graph[wire_to].append(wire_from)
+                except KeyError:
+                    self.graph[wire_to] = [wire_from]
 
-    def bfs_shortest_path(self, start: str, end: str) -> tuple[dict[str, float], dict[str, list[str]]]:
+    def bfs_shortest_path(self, start: str, end: str = "") -> dict[str, list[str]]:
         # A queue to manage the nodes to be checked, starts with the start node
         queue = deque([start])
 
+        # A set to skip already managed nodes
+        seen = set([start])
+
         # A dictionary to track distances to each node and the path taken
-        distances = {vertex: float('infinity') for vertex in self.graph}
-        distances[start] = 0
         paths: dict[str, list[str]] = {vertex: [] for vertex in self.graph}
         paths[start] = [start]
 
@@ -31,49 +38,47 @@ class Wires:
             vertex = queue.popleft()
 
             for neighbor in self.graph[vertex]:
-                if distances[neighbor] == float('infinity'):
-                    distances[neighbor] = distances[vertex] + 1
+                if neighbor not in seen:
+                    seen.add(neighbor)
                     paths[neighbor] = paths[vertex] + [neighbor]
                     queue.append(neighbor)
 
                     if neighbor == end:
-                        return distances, paths
+                        return paths
 
-        return distances, paths
+        return paths
 
     def split(self) -> int:
-        nodes = list(self.nodes)
+        nodes = sorted(self.nodes)
         bridges: dict[tuple[str, str], int] = defaultdict(int)
         if len(self.graph) > 100:
+            # Heuristic: check only (len(graph) - 1) pairs of nodes
             for node1, node2 in zip(nodes, nodes[1:]):
-                _, paths = self.bfs_shortest_path(node1, node2)
+                paths = self.bfs_shortest_path(node1, node2)
                 for node1, node2 in zip(paths[node2], paths[node2][1:]):
-                    bridges[(node1, node2)] += 1
+                    if node1 > node2:
+                        node2, node1 = node1, node2
+                    bridges[node1, node2] += 1
         else:
-            for node1, node2 in combinations(nodes, 2):
-                _, paths = self.bfs_shortest_path(node1, node2)
-                for nodeA, nodeB in zip(paths[node2], paths[node2][1:]):
-                    bridges[(nodeA, nodeB)] += 1
+            # Check all pairs of nodes
+            for node1, node2 in sorted(combinations(nodes, 2)):
+                paths = self.bfs_shortest_path(node1, node2)
+                for node1, node2 in zip(paths[node2], paths[node2][1:]):
+                    if node1 > node2:
+                        node2, node1 = node1, node2
+                    bridges[node1, node2] += 1
 
         bottlenecks = sorted(bridges.items(), key=itemgetter(1), reverse=True)
-        count = -1
-        for how_many in (3, 1, 1):
-            removed: set[tuple[str, str]] = set()
-            while len(removed) < how_many:
-                count += 1
-                node1, node2 = bottlenecks[count][0]
-                if node1 > node2:
-                    node1, node2, = node2, node1
-                if (node1, node2) not in removed:
-                    removed.add((node1, node2))
-                    self.graph[node1].remove(node2)
-                    self.graph[node2].remove(node1)
-                    if len(removed) == how_many:
-                        break
-            _, paths = self.bfs_shortest_path(node1, "")
-            detached = sum(1 for k, v in paths.items() if len(v) == 0)
-            if detached not in (0, len(self.graph)):
-                return (len(nodes) - detached) * detached
+        removed: set[tuple[str, str]] = set()
+        for count in range(3):
+            node1, node2 = bottlenecks[count][0]
+            removed.add((node1, node2))
+            self.graph[node1].remove(node2)
+            self.graph[node2].remove(node1)
+        paths = self.bfs_shortest_path(nodes[0], nodes[1])
+        detached = sum(1 for k, v in paths.items() if len(v) == 0)
+        if detached not in (0, len(self.graph)):
+            return (len(nodes) - detached) * detached
         raise Exception("Not found")
 
 
