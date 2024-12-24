ef9c1c8212c2988064dfce24d7ecf4133a5f9fcc
Marco Beri <marcoberi@gmail.com>
2024-12-24 09:53:22 +0100

    Day 24

---
+++ 2024/24/day_24.py
@@ -0,0 +1,162 @@
+from typing import TypeAlias, Dict, List, Tuple, Generator, Set, Optional, Union, Any  # noqa: F401
+from operator import and_, or_, xor
+from collections import defaultdict
+from itertools import combinations
+
+
+Changes: TypeAlias = Union[Tuple[int, ...], Tuple[()]]
+
+
+class Solution:
+    _instance = None
+
+    @classmethod
+    def get_instance(cls, data: str) -> "Solution":
+        if cls._instance is None:
+            cls._instance = Solution(data)
+        return cls._instance
+
+    def __init__(self, raw: str) -> None:
+        parts = raw.strip().split('\n\n')
+        self.values = {}
+        for line in parts[0].strip().splitlines():
+            p1, p2 = line.split(':')
+            self.values[p1] = int(p2.strip())
+        self.connections = defaultdict(list)
+        for line in parts[1].strip().splitlines():
+            p1, op, p2, p3 = line.replace('-> ', '').split(' ')
+            self.connections[(p1, {'AND': and_, 'XOR': xor, 'OR': or_}[op], p2)].append(p3)
+
+    def compute(self, connections: Dict[Tuple[str, Any, str], str], values: Dict[str, int]) -> int:
+        values = values.copy()
+        connections = connections.copy()
+        last_conn, last_val = (len(connections), len(values))
+        while connections:
+            for (p1, op, p2), p3s in connections.copy().items():
+                if not (p1 in values or p2 in values) and not (isinstance(p1, int) and isinstance(p2, int)):
+                    continue
+                connections.pop((p1, op, p2))
+                both = True
+                if not isinstance(p1, int):
+                    if p1 in values:
+                        p1 = values[p1]
+                    else:
+                        both = False
+                if not isinstance(p2, int):
+                    if p2 in values:
+                        p2 = values[p2]
+                    else:
+                        both = False
+                if both:
+                    for p3 in p3s:
+                        values[p3] = op(p1, p2)
+                else:
+                    connections[(p1, op, p2)] = p3s
+            if (last_conn, last_val) == (len(connections), len(values)):
+                return None
+            last_conn, last_val = (len(connections), len(values))
+        result, n = '', 0
+        while True:
+            label = 'z' + str(n).zfill(2)
+            if label not in values:
+                break
+            result = str(values[label]) + result
+            n += 1
+        return int(result, 2)
+
+    def wrong(self) -> str:
+        wires = len(self.values) // 2
+        wrong = set()
+        for n in range(wires):
+            values = self.values.copy()
+            for k in values:
+                values[k] = 0
+            lab_x = f"x{str(n).zfill(2)}"
+            lab_y = f"y{str(n).zfill(2)}"
+            values[lab_x] = 1
+            calc = self.compute(self.connections, values)
+            if calc is None:
+                return None
+            if calc != 2 ** n:
+                wrong.add(n)
+            values[lab_x] = 0
+            values[lab_y] = 1
+            calc = self.compute(self.connections, values)
+            if calc != 2 ** n:
+                wrong.add(n)
+            values[lab_x] = 1
+            calc = self.compute(self.connections, values)
+            if calc != 2 * 2 ** n:
+                wrong.add(n)
+        return wrong
+
+    def count(self) -> str:
+        return self.compute(self.connections, self.values)
+
+    def count2(self) -> str:
+        keys = list(self.connections.keys())
+        n_conn = len(keys)
+        improving = []
+        start_wrong = self.wrong()
+        for sw1, sw2 in combinations(range(n_conn), 2):
+            self.connections[keys[sw1]], self.connections[keys[sw2]] = self.connections[keys[sw2]], self.connections[keys[sw1]]
+            wrong = self.wrong()
+            if wrong is not None and len(wrong) < len(start_wrong):
+                improving.append((sw1, sw2))
+            self.connections[keys[sw1]], self.connections[keys[sw2]] = self.connections[keys[sw2]], self.connections[keys[sw1]]
+        # improving = [(30, 94),
+        #              (34, 157),
+        #              (57, 136),
+        #              (81, 136),
+        #              (87, 109),
+        #              (87, 177),
+        #              (104, 157),
+        #              (108, 136),
+        #              (134, 136),
+        #              (136, 164),
+        #              (136, 174),
+        #              (145, 157),
+        #              (157, 173),
+        #              (157, 206),
+        #              (168, 173),
+        #              (168, 206),
+        #              (173, 184)]
+        for swaps in combinations(improving, 4):
+            if len(set(wire for wires in swaps for wire in wires)) < 8:
+                continue
+            labels = []
+            for sw1, sw2 in swaps:
+                self.connections[keys[sw1]], self.connections[keys[sw2]] = self.connections[keys[sw2]], self.connections[keys[sw1]]
+                labels.append(self.connections[keys[sw1]][0])
+                labels.append(self.connections[keys[sw2]][0])
+            wrong = self.wrong()
+            if wrong is not None and len(wrong) == 0:
+                # ((30, 94), (81, 136), (87, 109), (157, 173))
+                # bqp,hbs,jcp,kfp,pdg,rfk,z18,z27
+                # ((30, 94), (81, 136), (87, 109), (168, 173))
+                # hbs,jcp,kfp,pdg,rfk,z18,z22,z27
+                # ((30, 94), (87, 109), (108, 136), (157, 173))
+                # bqp,dhq,hbs,jcp,kfp,pdg,z18,z27
+                # ((30, 94), (87, 109), (108, 136), (168, 173))
+                # dhq,hbs,jcp,kfp,pdg,z18,z22,z27            <- only this was right: WHY!?!
+                print(swaps)
+                print(",".join(sorted(labels)))
+                right = ",".join(sorted(labels))
+            for sw1, sw2 in swaps:
+                self.connections[keys[sw1]], self.connections[keys[sw2]] = self.connections[keys[sw2]], self.connections[keys[sw1]]
+        return right
+
+
+def solve1(data: str) -> int:
+    return Solution.get_instance(data).count()
+
+
+def solve2(data: str) -> str:
+    return Solution.get_instance(data).count2()
+
+
+if __name__ == "__main__":
+    import sys
+    data = open((sys.argv + ["input.txt"])[1]).read()
+    print(solve1(data))
+    print(solve2(data))
---
+++ 2024/24/test_24.py
@@ -0,0 +1,83 @@
+import os
+import unittest
+day = __import__('day_' + __file__[-5:-3])
+
+
+class TestDay(unittest.TestCase):
+    def setUp(self) -> None:
+        day.Solution._instance = None
+        self.data = open(os.path.dirname(__file__) + "/input.txt").read()
+        self.data_example = """x00: 1
+x01: 1
+x02: 1
+y00: 0
+y01: 1
+y02: 0
+
+x00 AND y00 -> z00
+x01 XOR y01 -> z01
+x02 OR y02 -> z02
+"""
+        self.data_example_2 = """x00: 1
+x01: 0
+x02: 1
+x03: 1
+x04: 0
+y00: 1
+y01: 1
+y02: 1
+y03: 1
+y04: 1
+
+ntg XOR fgs -> mjb
+y02 OR x01 -> tnw
+kwq OR kpj -> z05
+x00 OR x03 -> fst
+tgd XOR rvg -> z01
+vdt OR tnw -> bfw
+bfw AND frj -> z10
+ffh OR nrd -> bqk
+y00 AND y03 -> djm
+y03 OR y00 -> psh
+bqk OR frj -> z08
+tnw OR fst -> frj
+gnj AND tgd -> z11
+bfw XOR mjb -> z00
+x03 OR x00 -> vdt
+gnj AND wpb -> z02
+x04 AND y00 -> kjc
+djm OR pbm -> qhw
+nrd AND vdt -> hwm
+kjc AND fst -> rvg
+y04 OR y02 -> fgs
+y01 AND x02 -> pbm
+ntg OR kjc -> kwq
+psh XOR fgs -> tgd
+qhw XOR tgd -> z09
+pbm OR djm -> kpj
+x03 XOR y03 -> ffh
+x00 XOR y04 -> ntg
+bfw OR bqk -> z06
+nrd XOR fgs -> wpb
+frj XOR qhw -> z04
+bqk OR frj -> z07
+y03 OR x01 -> nrd
+hwm AND bqk -> z03
+tgd XOR rvg -> z12
+tnw OR pbm -> gnj
+"""
+
+    def test_part_1_A(self) -> None:
+        self.assertEqual(day.solve1(self.data_example), 4)
+
+    def test_part_1_B(self) -> None:
+        self.assertEqual(day.solve1(self.data_example_2), 2024)
+
+    def _test_part_2(self) -> None:
+        self.assertEqual(day.solve2(self.data_example), 'co,de,ka,ta')
+
+    def test_solutions_1(self) -> None:
+        self.assertEqual(day.solve1(self.data), 69201640933606)
+
+    def _test_solutions_2(self) -> None:
+        self.assertEqual(day.solve2(self.data), "")
