146e8b4c8cb9e605448377f96d4b2a0230c5cdcb
Marco Beri <marcoberi@gmail.com>
2025-12-11 20:18:06 +0100

    fix test

--- 2025/11/test_11.py
+++ 2025/11/test_11.py
@@ -17,23 +17,26 @@ ggg: out
 hhh: ccc fff iii
 iii: out
 """
-        self.data_example = """aaa: you hhh
-you: bbb ccc
-bbb: ddd eee
-ccc: ddd eee fff
-ddd: ggg
-eee: out
-fff: out
+        self.data_example_2 = """svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
 ggg: out
-hhh: ccc fff iii
-iii: out
+hhh: out
 """
 
     def test_part_1(self) -> None:
         self.assertEqual(5, day.solve1(self.data_example))
 
     def test_part_2(self) -> None:
-        self.assertEqual(2, day.solve2(self.data_example))
+        self.assertEqual(2, day.solve2(self.data_example_2))
 
     def test_solutions(self) -> None:
         self.assertEqual(day.solve1(self.data), 590)
